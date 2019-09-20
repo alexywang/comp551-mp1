edf906e9005716bd942c6b07e09f07dfe7e7d5cd
David Carrier <david.carrier@mail.mcgill.ca>
2019-09-19 22:57:59 -0400

    Added methods to normalize data and to add extra data features (squares, products)

--- data/wine/wine_import.py
+++ data/wine/wine_import.py
@@ -1,6 +1,12 @@
 import pandas as pd
 
 def get_data():
+    """
+        Reads data from winequality-red.csv and applies treatment to the "quality" feature
+        in order to turn it into a binary category.
+
+        The "quality" feature is thus renamed "quality_bin"
+    """
     raw_data = pd.read_csv("winequality-red.csv", sep=";")
     treated_data = raw_data
 

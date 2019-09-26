2da6928c35bc8eb4ffe3bc50a877871a03ade393
Elie Climan <33531767+ecliman@users.noreply.github.com>
2019-09-26 13:58:30 -0400

    Update bc_import.py

--- root/bc_import.py
+++ root/bc_import.py
@@ -6,10 +6,11 @@ def get_data():
         reads data from breast cancer data document. Adds data to treated_data, finds the last column which
         indicates Malignant or Benign and drops it.
     """
-    raw_data = pd.read_csv("breast-cancer-wisconsin.csv", sep=";")
-    treated_data = raw_data
-    columns = treated_data.columns.tolist()  # get the columns
-    cols_to_use = columns[:len(columns) - 1]  # drop the last one
-    treated_data = read_csv(path, usecols=cols_to_use)
+    raw_data = pd.read_csv("breast-cancer-wisconsin.csv")
+    treated_data = (raw_data[raw_data.columns[:-1]])
+    treated_data = (treated_data.loc[treated_data["compactness"] != '?'])
+    treated_data.reset_index(drop=True, inplace=True)
     return treated_data
 
+
+print(get_data())
